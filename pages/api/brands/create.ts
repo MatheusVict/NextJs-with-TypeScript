91b3b84f68ad8fdd574544d6bbeb7fd26795dfc5
MatheusVict <matheusvictorhenrique@gmail.com>
2023-01-21 10:45:57 -0300

    connection with prisma in endpoint

--- pages/api/brands/create.ts
+++ pages/api/brands/create.ts
@@ -1,17 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '@/lib/prisma'
+import { Brand } from '@prisma/client';
 
-type Data = {
-  name: string
-}
+
 
 type ErrorData = {
   error: string;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data | ErrorData>
+  res: NextApiResponse<Brand | ErrorData>
 ) {
   if (req.method == 'POST') {
 
@@ -20,7 +20,13 @@ export default function handler(
 
       if(!name) throw new Error('Propiedade name ausente')
 
-      res.status(201).json({ name: 'Brands' })
+      const createBrands = await prisma.brand.create({
+        data: {
+          name
+        }
+      })
+
+      res.status(201).json(createBrands)
     } catch (error) {
       res.status(500).json({error: 'internal Server Error'})
     }
